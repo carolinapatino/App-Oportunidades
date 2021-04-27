248703ed43f982a1597a77f63df94f452eea41f0
carolinapatino <carolina.patino@galac.com>
2021-04-27 01:38:09 -0400

    Inserts definitivs

--- Venezuela/Oportunidades/SPCerrarOportunidad.sql
+++ Venezuela/Oportunidades/SPCerrarOportunidad.sql
@@ -25,6 +25,23 @@ CREATE PROC [dbo].[CerrarOportunidad](@jsonCierre NVARCHAR(500), @OportunidadId 
   FechaReapertura = c.FechaReapertura
   FROM JSONCierre c
   WHERE OportunidadId = @OportunidadId 
+
+  
+SELECT O.OportunidadId, O.FechaCreacion, O.FechaCierre, O.MontoPresupuesto, O.Objetivo, O.ObservacionDeCierre, A.OrigenNombre, E.NombreEstatus,
+ O.CodigoCliente,O.NombreCliente, O.IdCreador, O.IdVendedor, STRING_AGG (P.ProductoId, ',') AS Productos, O.idUsuarioCerrador, O.FechaReapertura
+FROM OportunidadDeVenta O, 
+		Estatus E, 
+		Origen A, 
+		ProductoOportunidad P, 
+		Producto Pr
+ WHERE  E.EstatusId = O.Estatus 
+		AND O.Origen = A.OrigenId 
+		AND P.NroOportunidad = O.OportunidadId 
+		AND Pr.ProductoId = P.ProductoId 
+		AND O.OportunidadId = @OportunidadId
+GROUP BY P.NroOportunidad, O.OportunidadId, O.FechaCreacion, O.FechaCierre, O.MontoPresupuesto, O.Objetivo, O.ObservacionDeCierre, A.OrigenNombre, E.NombreEstatus,
+O.CodigoCliente,O.NombreCliente, O.IdCreador, O.IdVendedor, O.idUsuarioCerrador, O.FechaReapertura
+
  end
 
  
--- Venezuela/Oportunidades/SPEditarOportunidad.sql
+++ Venezuela/Oportunidades/SPEditarOportunidad.sql
@@ -96,4 +96,20 @@ CREATE PROC [dbo].[EditarOportunidad] (@jsonOportunidad NVARCHAR(500), @Oportuni
 
 
 exec CrearRecordatorio @UltimaAccion
+
+SELECT O.OportunidadId, O.FechaCreacion, O.FechaCierre, O.MontoPresupuesto, O.Objetivo, O.ObservacionDeCierre, A.OrigenNombre, E.NombreEstatus,
+ O.CodigoCliente,O.NombreCliente, O.IdCreador, O.IdVendedor, STRING_AGG (P.ProductoId, ',') AS Productos, O.idUsuarioCerrador, O.FechaReapertura
+FROM OportunidadDeVenta O, 
+		Estatus E, 
+		Origen A, 
+		ProductoOportunidad P, 
+		Producto Pr
+ WHERE  E.EstatusId = O.Estatus 
+		AND O.Origen = A.OrigenId 
+		AND P.NroOportunidad = O.OportunidadId 
+		AND Pr.ProductoId = P.ProductoId 
+		AND O.OportunidadId = @OportunidadId
+GROUP BY P.NroOportunidad, O.OportunidadId, O.FechaCreacion, O.FechaCierre, O.MontoPresupuesto, O.Objetivo, O.ObservacionDeCierre, A.OrigenNombre, E.NombreEstatus,
+O.CodigoCliente,O.NombreCliente, O.IdCreador, O.IdVendedor, O.idUsuarioCerrador, O.FechaReapertura
+
  end
